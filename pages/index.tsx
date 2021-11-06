8127a4cca3d27b92dc05f83197badaa3312e32e5
Jason Bamford <47010074+jb-23@users.noreply.github.com>
2021-11-06 14:36:17 +0000

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout"
+
+const IndexPage = () => (
+  <Layout title="Home | Next.js + TypeScript Example">
+    <h1>Welcome to <a href="https://nextjs.org">Next.js!</a></h1>
+    <div>
+    Page content.
+    </div>
+  </Layout>
+)
+
+export default IndexPage
